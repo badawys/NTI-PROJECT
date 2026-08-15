5832194bebfe7d403203031c785a7da7b3826ae6
Ahmad Elbadawy <ahmad.elbadawy@outlook.com>
2026-08-15 18:00:26 +0300

    first commit

---
+++ api/src/config/database.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export async function connectToDatabase(mongoUri: string): Promise<void> {
+
+  await mongoose.connect(mongoUri);
+
+  console.log("MongoDB connected");
+}
